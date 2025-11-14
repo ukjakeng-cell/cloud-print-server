f827aa45cf8142b332abf0209943b5b1f2a3f5d6
JakeNum5 <120614458+JakeNum5@users.noreply.github.com>
2025-11-14 02:18:36 +0000

    update db setting

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 import express from "express";
 import cors from "cors";
+import pool from "./db.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Testing API
 app.get("/", (req, res) => {
-  res.send("Cloud Print Server Running on Windows!");
+  res.send("Cloud Print Server Running!");
+});
+
+// DB 測試
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ server: "OK", time: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "DB connection failed" });
+  }
 });
 
 app.listen(3000, () => {
